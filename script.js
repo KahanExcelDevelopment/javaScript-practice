147318a4609c41be22ab9e863a48763d722ee528
Alexandre Armando <armando.alexandrej@gmail.com>
2025-07-02 22:31:34 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// this is an assignment to test my knowledge in varribles//
+
+// Create three variables Your favorite food Your favorite color Your favorite numberThen log them using console.log().
+
+// first i will create a varriable to hold message
+
+// create a function
+function updateMessage() {
+  prompt("shalam priest");
+}
+
+updateMessage();
