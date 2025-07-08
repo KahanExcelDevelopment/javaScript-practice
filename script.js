89c57d669cc8833591d8dd0458e3f893645cb83b
Alexandre Armando <armando.alexandrej@gmail.com>
2025-07-07 21:18:40 -0400

    declaring functions that need info

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
-// this is an assignment to test my knowledge in varribles//
+// declRING FUNCTIONS THAT NEEDS IN FROMATION
+let south = "3";
+let north = "9";
 
-// Create three variables Your favorite food Your favorite color Your favorite numberThen log them using console.log().
-
-// first i will create a varriable to hold message
-
-// create a function to make 180k
-
-let zeal = "180k";
-let msg = "you are the man ahkal";
-
-function updateMessage() {
-  let el = document.getElementById("message");
-  el.textContent = msg;
+function Tribes(s, n) {
+  return s + n;
 }
 
-updateMessage();
+console.log(Tribes(south, north)); // Outputs: 39
