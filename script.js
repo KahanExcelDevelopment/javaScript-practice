29a54dc637f7cf1e582f05ff06a021a2a04572cf
Alexandre Armando <armando.alexandrej@gmail.com>
2025-07-09 20:48:04 -0400

    2nd single value out of a function practice

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
-// Getting a single value out of an function
-// 📘 Assignment: “Tribe Tracker” — Return the Number of Men in Each Tribe Mission
-// You are building a simple script to track the number of men registered in each tribe of Israel, and return totals when asked.
+//  2. getMenInTribe(tribeName) Takes a tribe name as input, and returns the number of men in that tribe.
 
-//  1. getTribeCount() Returns: the total number of tribes, should return 12
+let judah = "74,600";
 
-let northernTribes = 3;
-
-let southernTribes = 9;
-
-function totalNumberOfTribes(north, south) {
-  return north + south;
+function getMenInTribe(jew) {
+  return jew;
 }
 
-console.log(totalNumberOfTribes(northernTribes, southernTribes));
+console.log(getMenInTribe(judah));
